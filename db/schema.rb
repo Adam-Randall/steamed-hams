1255e4ec728672199f958a512a92f23acc54e2f0
Adam Randall <ajp.randall@gmail.com>
2015-08-18 16:53:39 +1200

    changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722074826) do
+ActiveRecord::Schema.define(version: 20150818013714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "encrypted_password"
-    t.string   "salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "share_actions", force: :cascade do |t|
+    t.integer  "share_id"
+    t.float    "current_value"
+    t.string   "week_change"
+    t.string   "year_change"
+    t.string   "trading_status"
+    t.string   "trades"
+    t.float    "value"
+    t.string   "volume"
+    t.float    "capitalisation"
+    t.float    "open"
+    t.float    "high"
+    t.float    "low"
+    t.float    "high_bid"
+    t.float    "low_offer"
+    t.string   "shares_issued"
+    t.string   "gross_div_yield"
+    t.float    "nta"
+    t.float    "eps"
+    t.string   "pe"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
+  add_index "share_actions", ["share_id"], name: "index_share_actions_on_share_id", using: :btree
+
+  create_table "shares", force: :cascade do |t|
+    t.string   "name",            null: false
+    t.string   "code",            null: false
+    t.string   "issued_by"
+    t.string   "isin"
+    t.string   "security_type"
+    t.string   "sector"
+    t.string   "website_address"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "user_shares", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "share_actions", "shares"
 end
